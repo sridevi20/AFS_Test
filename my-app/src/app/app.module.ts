6da7705a835db301b8b10efcce634745f4b57e46
sridevi <sdara20@gmail.com>
2019-08-25 21:50:04 +0100

    Adding  new ANT UI library again

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HomeComponent } from './home/home/home.component';
 import { DriversComponent } from './home/drivers/drivers.component';
 import { DocumentsComponent } from './home/documents/documents.component';
 import { AuthComponent } from './auth/auth.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 
 
@@ -28,12 +36,16 @@ import { AuthComponent } from './auth/auth.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
 
 
 
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
