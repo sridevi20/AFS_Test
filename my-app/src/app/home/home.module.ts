1d1dff4389d10fb250d80e0d9d2fdf8829e81036
sridevi <sdara20@gmail.com>
2019-08-26 04:17:50 +0100

    fixes error unexpected expression

--- my-app/src/app/home/home.module.ts
+++ my-app/src/app/home/home.module.ts
@@ -4,11 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { DriversComponent } from './drivers/drivers.component';
 import { DocumentsComponent } from './documents/documents.component';
 
-
-{
-  path: 'home',
-  loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule),
-},
+//
+// {
+//   path: 'home',
+//   loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule),
+// },
 
 @NgModule({
   declarations: [HomeComponent, DriversComponent, DocumentsComponent],
